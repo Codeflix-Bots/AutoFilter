3d59fc3e6b23742a5bea5bdd8526aecac6b37160
Codeflix-Bots <159244479+Codeflix-Bots@users.noreply.github.com>
2024-03-26 10:21:15 +0530

    Add files via upload

---
+++ plugins/more/wish.py
@@ -0,0 +1,62 @@
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+import random
+import requests
+
+SUPPORT_CHAT = "weebs_support"
+
+@Client.on_message(filters.command("wish"))
+async def wish(_, m):
+    if len(m.command) < 2:
+        await m.reply("ᴀᴅᴅ ᴡɪꜱʜ ʙᴀʙʏ🥀!")
+        return 
+
+    api = requests.get("https://nekos.best/api/v2/happy").json()
+    url = api["results"][0]['url']
+    text = m.text.split(None, 1)[1]
+    wish_count = random.randint(1, 100)
+    wish = f"✨ ʜᴇʏ! {m.from_user.first_name}! "
+    wish += f"✨ ʏᴏᴜʀ ᴡɪꜱʜ: {text} "
+    wish += f"✨ ᴘᴏꜱꜱɪʙʟᴇ ᴛᴏ: {wish_count}%"
+    
+    await Client.send_animation(
+        chat_id=m.chat.id,
+        animation=url,
+        caption=wish,
+        reply_markup=InlineKeyboardMarkup(
+            [[InlineKeyboardButton("ꜱᴜᴘᴘᴏʀᴛ", url=f"https://t.me/weebs_support")]])
+    )
+            
+    
+BUTTON = [[InlineKeyboardButton("ꜱᴜᴘᴘᴏʀᴛ", url=f"https://t.me/weebs_support")]]
+CUTIE = "https://64.media.tumblr.com/d701f53eb5681e87a957a547980371d2/tumblr_nbjmdrQyje1qa94xto1_500.gif"
+
+@Client.on_message(filters.command("cute"))
+async def cute(_, message):
+    if not message.reply_to_message:
+        user_id = message.from_user.id
+        user_name = message.from_user.first_name
+    else:
+        user_id = message.reply_to_message.from_user.id
+        user_name = message.reply_to_message.from_user.first_name
+
+    mention = f"[{user_name}](tg://user?id={str(user_id)})"
+    mm = random.randint(1, 100)
+    CUTE = f"🍑 {mention} {mm}% ᴄᴜᴛᴇ ʙᴀʙʏ🥀"
+
+    await Client.send_document(
+        chat_id=message.chat.id,
+        document=CUTIE,
+        caption=CUTE,
+        reply_markup=InlineKeyboardMarkup(BUTTON),
+        reply_to_message_id=message.reply_to_message.message_id if message.reply_to_message else None,
+    )
+    
+help_text = """
+» ᴡʜᴀᴛ ɪꜱ ᴛʜɪꜱ (ᴡɪꜱʜ):
+ʏᴏᴜ ʜᴀᴠɪɴɢ ᴀɴʏ ᴋɪɴᴅ ᴏꜰ 
+(ᴡɪꜱʜᴇꜱ) ʏᴏᴜ ᴄᴀɴ ᴜꜱɪɴɢ ᴛʜɪꜱ ʙᴏᴛ ᴛᴏ ʜᴏᴡ ᴘᴏꜱꜱɪʙʟᴇ ᴛᴏ ʏᴏᴜʀ ᴡɪꜱʜ!
+ᴇxᴀᴍᴘʟᴇ:» /wish : ɪ ᴡᴀɴᴛ ᴄʟᴀꜱꜱ ᴛᴏᴘᴘᴇʀ 
+» /wish : ɪ ᴡᴀɴᴛ ᴀ ɴᴇᴡ ɪᴘʜᴏɴᴇ 
+» /cute : ʜᴏᴡ ᴍᴜᴄʜ ɪ ᴀᴍ ᴄᴜᴛᴇ 
+"""
