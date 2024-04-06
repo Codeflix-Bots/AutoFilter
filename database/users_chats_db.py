98521b85d16c5f02946362a55fdf9b890d1b4cff
Codeflix-Bots <159244479+Codeflix-Bots@users.noreply.github.com>
2024-04-06 12:30:27 +0530

    Update users_chats_db.py

--- database/users_chats_db.py
+++ database/users_chats_db.py
@@ -1,8 +1,38 @@
-# https://github.com/odysseusmax/animated-lamp/blob/master/bot/database/database.py
+#codeflix-bots
+
 import motor.motor_asyncio
+from pymongo.errors import DuplicateKeyError
+from pymongo import MongoClient
 from info import DATABASE_NAME, DATABASE_URI, IMDB, IMDB_TEMPLATE, MELCOW_NEW_USERS, P_TTI_SHOW_OFF, SINGLE_BUTTON, SPELL_CHECK_REPLY, PROTECT_CONTENT, AUTO_DELETE, MAX_BTN, AUTO_FFILTER, SHORTLINK_API, SHORTLINK_URL, IS_SHORTLINK, TUTORIAL, IS_TUTORIAL
 import datetime
 import pytz
+
+my_client = MongoClient(DATABASE_URI)
+mydb = my_client["referal_user"]
+
+async def referal_add_user(user_id, ref_user_id):
+    user_db = mydb[str(user_id)]
+    user = {'_id': ref_user_id}
+    try:
+        user_db.insert_one(user)
+        return True
+    except DuplicateKeyError:
+        return False
+    
+
+async def get_referal_all_users(user_id):
+    user_db = mydb[str(user_id)]
+    return user_db.find()
+    
+async def get_referal_users_count(user_id):
+    user_db = mydb[str(user_id)]
+    count = user_db.count_documents({})
+    return count
+    
+
+async def delete_all_referal_users(user_id):
+    user_db = mydb[str(user_id)]
+    user_db.delete_many({}) 
 
 class Database:
     
