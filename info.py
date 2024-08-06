dad31765907aad97e4ea5652874835e47c99ce09
𝖢𝗈𝖽𝖾𝖿𝗅𝗂𝗑 𝖡𝗈𝗍𝗌 <159244479+Codeflix-Bots@users.noreply.github.com>
2024-08-06 20:45:33 +0530

    Update info.py

--- info.py
+++ info.py
@@ -11,35 +11,35 @@ def is_enabled(value, default):
         return default
 
 # Bot information
-API_ID = environ.get('API_ID')
-API_HASH = environ.get('API_HASH')
-BOT_TOKEN = environ.get('BOT_TOKEN')
+API_ID = environ.get('API_ID', "")
+API_HASH = environ.get('API_HASH', "")
+BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', "990262535").split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', "6497757690").split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS',).split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 auth_channel = environ.get('AUTH_CHANNEL')
-auth_grp = environ.get('AUTH_GROUP', "-1001633071686")
+auth_grp = environ.get('AUTH_GROUP', "")
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 SUPPORT_CHANNEL = environ.get('CHANNEL_USERNAME')
 SUPPORT_GROUP = environ.get('GROUP_USERNAME')
-INDEX_USER = [int(environ.get('INDEX_USER', '1247742004'))]
+INDEX_USER = [int(environ.get('INDEX_USER', '6497757690'))]
 INDEX_USER.extend(ADMINS)
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI')
-DATABASE_NAME = environ.get('DATABASE_NAME', "wolve")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'wolve')
+DATABASE_URI = environ.get('DATABASE_URI', "")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'TgFiles')
 
 # Others
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL'))
-FORCESUB_CHANNEL = int(environ.get('FORCESUB_CHANNEL'))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', "-1001868871195"))
+FORCESUB_CHANNEL = int(environ.get('FORCESUB_CHANNEL', "-1001473043276"))
 SLOW_MODE_DELAY = int(environ.get('SLOW_MODE_DELAY', 60))
 WAIT_TIME = int(environ.get('AUTO_DELETE_WAIT_TIME', 1800))
-FORWARD_CHANNEL = int(environ.get('FORWARD_CHANNEL', "-1002123504264"))
+FORWARD_CHANNEL = int(environ.get('FORWARD_CHANNEL', "-1001868871195"))
 
 # other
 ACCESS_KEY = environ.get("LICENSE_ACCESS_KEY")
