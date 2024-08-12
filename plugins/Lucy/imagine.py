0e720941ca19104f4a0b0148bdfbdebea8ea3424
𝖢𝗈𝖽𝖾𝖿𝗅𝗂𝗑 𝖡𝗈𝗍𝗌 <159244479+Codeflix-Bots@users.noreply.github.com>
2024-08-12 13:17:47 +0530

    Update imagine.py

--- plugins/Lucy/imagine.py
+++ plugins/Lucy/imagine.py
@@ -1,33 +1,54 @@
-from pyrogram import Client, filters
-from pyrogram.types import  Message
-from pyrogram.types import InputMediaPhoto
-from MukeshAPI import api
-from pyrogram.enums import ChatAction,ParseMode
+import requests
+import time
+import os
+from pyrogram import filters, Client 
 
-@Client.on_message(filters.command("imagine"))
-async def imagine_(b, message: Message):
-    if message.reply_to_message:
-        text = message.reply_to_message.text
+# Command handler for /gen
+@Client.on_message(filters.command(['imagine','generate']))
+async def generate_image(client, message):
+    # Get the prompt from the command
+    prompt = ' '.join(message.command[1:])
+
+    # Send a message to inform the user to wait
+    wait_message = await message.reply_text("Please wait while I generate the image...")
+    StartTime = time.time()
+
+
+    # API endpoint URL
+    url = 'https://ai-api.magicstudio.com/api/ai-art-generator'
+
+    # Form data for the request
+    form_data = {
+        'prompt': prompt,
+        'output_format': 'bytes',
+        'request_timestamp': str(int(time.time())),
+        'user_is_subscribed': 'false',
+    }
+
+    # Send a POST request to the API
+    response = requests.post(url, data=form_data)
+
+    if response.status_code == 200:
+        try:
+            if response.content:
+                destination_dir = ''
+                destination_path = os.path.join(destination_dir, 'generated_image.jpg')
+
+                # Save the image to the destination path
+                with open(destination_path, 'wb') as f:
+                    f.write(response.content)
+
+                # Delete the wait message
+                await wait_message.delete()
+
+                # Send the generated image
+                await message.reply_photo(destination_path, caption=f"Here's the generated image!\nTime Taken: {time.time() - StartTime}")
+
+                # Delete the generated image after sending
+                os.remove(destination_path)
+            else:
+                await wait_message.edit_text("Failed to generate the image.")
+        except Exception as e:
+            await wait_message.edit_text("Error: {}".format(e))
     else:
-
-        text =message.text.split(None, 1)[1]
-    Lucy=await message.reply_text( "`ᴘʟᴇᴀsᴇ ᴡᴀɪᴛ ʙʀᴏ...\n\nɢᴇɴᴇʀᴀᴛɪɴɢ ɪᴍᴀɢe...`")
-    try:
-        await b.send_chat_action(message.chat.id, ChatAction.UPLOAD_PHOTO)
-        x=api.ai_image(text)
-        with open("lucy.jpg", 'wb') as f:
-            f.write(x)
-        caption = f"""
-    ⌯ sᴜᴄᴇssғᴜʟʟʏ ɢᴇɴᴇʀᴀᴛᴇᴅ : {text}
-    ⌯ ɢᴇɴᴇʀᴀᴛᴇᴅ ʙʏ : @lucy_filter_bot
-    """
-        await Lucy.delete()
-        await message.reply_photo("lucy.jpg",caption=caption,quote=True)
-    except Exception as e:
-        await Lucy.edit_text(f"error {e}")
-    
-
-__mod_name__ = "Aɪ ɪᴍᴀɢᴇ"
-__help__ = """
- ➻ /imagine : ɢᴇɴᴇʀᴀᴛᴇ Aɪ ɪᴍᴀɢᴇ ғʀᴏᴍ ᴛᴇxᴛ
- """
+        await wait_message.edit_text("Error: {}".format(response.status_code))
